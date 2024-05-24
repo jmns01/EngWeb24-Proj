17de2976c0a7b9559819147b77fc94681419b0c4
João Silva <a91671@alunos.uminho.pt>
2024-05-24 02:24:34 +0100

    Database ready to work

--- analise.py
+++ analise.py
@@ -1,20 +1,34 @@
-import csv
+import json
+from collections import defaultdict
 
-def tipos_desc(file):
-    descs=[]
-    file.readline() # skip da primeira
-    info = file.readlines()
-    for line in info:
-        partes = line.split(';')
-        print(partes)
-        if partes[1] not in descs:
-            descs.append(partes[1])
-    return descs
+def make_hashable(value):
+    """ Convert lists to tuples recursively to ensure they are hashable. """
+    if isinstance(value, list):
+        return tuple(make_hashable(item) for item in value)
+    return value
 
-def n_colunas(file):
-    first = file.readline()
-    return len(first.split(";"))
+def list_unique_values(json_filename):
+    with open(json_filename, 'r', encoding='utf-8') as f:
+        data = json.load(f)
 
-f = open("./PT-UM-ADB-DIO-MAB-006.csv", "r", encoding="utf-8")
-#tipos_desc(f)
-print("Número de propriedades: " + str(n_colunas(f)))
+    unique_values = defaultdict(set)
+
+    for entry in data:
+        for key, value in entry.items():
+            if isinstance(value, list):
+                for item in value:
+                    unique_values[key].add(make_hashable(item))
+            else:
+                unique_values[key].add(make_hashable(value))
+    
+    unique_values = {key: list(values) for key, values in unique_values.items()}
+
+    return unique_values
+
+def save_unique_values_to_json(unique_values, output_filename):
+    with open(output_filename, 'w', encoding='utf-8') as f:
+        json.dump(unique_values, f, ensure_ascii=False, indent=4)
+
+unique_values = list_unique_values('inquiricoes.json')
+save_unique_values_to_json(unique_values, 'unique_values.json')
+
--- csv_json_parser.py
+++ csv_json_parser.py
@@ -1,7 +1,11 @@
 import csv
 import json
+import re
 
-def read_csv(filename):
+def read_csv(filename : str) -> list:
+    """
+    Reads csv file and organizes everything in a list of dictionaries
+    """
     data = []
     with open(filename, 'r', encoding='utf-8-sig') as csvfile:
         reader = csv.DictReader(csvfile, delimiter=';')
@@ -12,7 +16,10 @@ def read_csv(filename):
             data.append(row)
     return data
 
-def switch_id(json_dic):
+def switch_id(json_dic : dict) -> dict:
+    """
+    Ranames the 'ID' key to '_id' to be compatible with mongoDB
+    """
     new = []
     for elem in json_dic:
         row = elem
@@ -20,11 +27,69 @@ def switch_id(json_dic):
         new.append(row) 
     return new
 
-def convert_to_json(csv_data, json_filename):
+def fix_types(json_dic : dict) -> dict:
+    """
+    Removes quotes from values that should be different types
+    """
+    new = []
+    for row in json_dic:
+        elem = row
+        elem['_id'] = int(elem['_id'])
+        elem['UnitId'] = int(elem['UnitId'])
+        elem['UnitDateInitialCertainty'] = bool(elem['UnitDateInitialCertainty'])
+        elem['UnitDateFinalCertainty'] = bool(elem['UnitDateFinalCertainty'])
+        elem['AllowUnitDatesInference'] = bool(elem['AllowUnitDatesInference'])
+        elem['AllowExtentsInference'] = bool(elem['AllowExtentsInference'])
+        elem['AllowTextualContentInference'] = bool(elem['AllowTextualContentInference'])
+        elem['ApplySelectionTable'] = bool(elem['ApplySelectionTable'])
+        elem['Revised'] = bool(elem['Revised'])
+        elem['Published'] = bool(elem['Published'])
+        elem['Available'] = bool(elem['Available'])
+        elem['Highlighted'] = bool(elem['Highlighted'])
+
+        new.append(elem)
+    return new
+
+def make_relations(json_dic : dict) -> dict:
+    """
+    Analyses the 'ScopeContent' field which contains information regarding genealogical relationships of the person
+    """
+    names_pattern = re.compile(r'Filiação:((?:\s*\w{2,})+)(?:\s*e\s*((?:\s*\w{2,})+))?.')
+    
+    name_to_id = {}
+    for row in json_dic:
+        name = row["UnitTitle"].lstrip("Inquirição de genere de").strip()
+        name_to_id[name] = row["_id"]
+    
+    for row in json_dic:
+        elem = row
+        elem["Relations"] = []
+        
+        match = names_pattern.search(row["ScopeContent"])
+        
+        if not match:
+            print("Inquirição sem relações")
+        else:
+            for i in range(1, 3):
+                if match.group(i):
+                    name = match.group(i).strip()
+                    relation_id = name_to_id.get(name, 0)
+                    if relation_id:
+                        elem["Relations"].append(relation_id)
+        
+    return json_dic
+
+
+def write_to_json(json_dic : dict, json_filename : str) -> None:
+    """
+    Writes dictonary to file
+    """
     with open(json_filename, 'w', encoding='utf-8') as jsonfile:
-        json.dump(csv_data, jsonfile, ensure_ascii=False, indent=4)
+        json.dump(json_dic, jsonfile, ensure_ascii=False, indent=4)
 
 
 csv_data = read_csv('PT-UM-ADB-DIO-MAB-006.csv')
 id_fixed = switch_id(csv_data)
-convert_to_json(id_fixed, 'inquiricoes.json')
+types = fix_types(id_fixed)
+relations = make_relations(types)
+write_to_json(relations, 'inquiricoes.json')

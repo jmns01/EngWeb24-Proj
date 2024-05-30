6ebc37c2e738935dbe22bb084b96238c02c037a3
Gnomo03 <a100554@alunos.uminho.pt>
2024-05-30 21:20:56 +0100

    auth (not working)

---
+++ auth/app.js
@@ -0,0 +1,51 @@
+var createError = require('http-errors');
+var express = require('express');
+var logger = require('morgan');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+var mongoose = require('mongoose');
+
+// Configuração do banco de dados
+var mongoDB = 'mongodb://127.0.0.1/inquiricoes';
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'Erro de conexão ao MongoDB...'));
+db.once('open', function() {
+  console.log("Conexão ao MongoDB realizada com sucesso...");
+});
+
+// Configuração do Passport
+var User = require('./models/user'); // Certifique-se de que este caminho está correto
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+var usersRouter = require('./routes/user'); // Certifique-se de que este caminho está correto
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use('/users', usersRouter);
+
+// Tratamento de erro 404
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// Manipulador de erros
+app.use(function(err, req, res, next) {
+  // Definir variáveis locais, fornecendo apenas erros em desenvolvimento
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // Renderizar a página de erro
+  res.status(err.status || 500);
+  res.jsonp({ error: err });
+});
+
+module.exports = app;
---
+++ auth/controllers/user.js
@@ -0,0 +1,88 @@
+var User = require('../models/user')
+
+
+module.exports.find = function() {
+    return User.find().exec();
+};
+  
+module.exports.getUser = function(username) {
+    return User.findOne({ username: username }).exec();
+};
+
+module.exports.addUser = u => {
+    return User.create(u)
+        .then(resposta => {
+            return resposta
+        })
+        .catch(erro => {
+            return erro
+        })
+}
+
+module.exports.loginUser = (username, data) => {
+    return User.updateOne({username: username}, {$set: {lastAccess: data}})
+        .then(resposta => {
+            return resposta
+        })
+        .catch(erro => {
+            return erro
+        })
+}
+
+module.exports.updateUser = newUser => {
+    return User.updateOne({username: newUser.username}, newUser)  
+        .then(resposta => {
+            return resposta
+        })
+        .catch(erro => {
+            return erro
+        })
+}
+
+module.exports.deleteUser = username => {
+    return User.deleteOne({username:username})
+    .then(dados=>{
+        return dados
+    })
+    .catch(erro =>{
+        return erro
+    })
+}
+
+module.exports.deactivateUser = async (username) => {
+    try {
+      const updatedUser = await User.findOneAndUpdate(
+        { username },
+        { $set: { active: false } },
+        { new: true }
+      );
+  
+      if (!updatedUser) {
+        throw new Error('Utilizador não encontrado.');
+      }
+  
+      return updatedUser;
+    } catch (error) {
+      console.error('Erro ao desativar o utilizador:', error);
+      throw new Error('Ocorreu um erro ao processar a solicitação.');
+    }
+};
+
+module.exports.activateUser = async (username) => {
+    try {
+      const updatedUser = await User.findOneAndUpdate(
+        { username },
+        { $set: { active: true } },
+        { new: true }
+      );
+  
+      if (!updatedUser) {
+        throw new Error('Utilizador não encontrado.');
+      }
+  
+      return updatedUser;
+    } catch (error) {
+      console.error('Erro ao ativar o utilizador:', error);
+      throw new Error('Ocorreu um erro ao processar a solicitação.');
+    }
+  };

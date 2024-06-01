fe0af1c06debf8e0ea3ecbf7136a41886ca00f2a
andrepfilipe01 <andre.filipe_01@hotmail.com>
2024-06-01 12:42:56 +0100

    pagina Login e verificaToken para o auth

--- interfaceServer/routes/index.js
+++ interfaceServer/routes/index.js
@@ -2,6 +2,22 @@ var express = require('express');
 var router = express.Router();
 var axios = require('axios');
 
+function verificaToken(req, res, next){
+  if(req.cookies && req.cookies.token){
+    jwt.verify(req.cookies.token, "PGDRE2023", function(e, payload){
+      if(e){// Erro na validação do token
+        res.render('error', {error: "O token do pedido não é válido...", token: false})
+      }
+      else{ // Só avança se existir um token e se este for verificado com sucesso
+        req.user = payload // Informações do user -> req.user
+        next()
+      }
+    })
+  }else{ // Não existe token
+    res.render('error', {error: "O pedido não tem um token...", token: false})
+  }
+}
+
 /* GET home page. */
 router.get('/getInquiricoesList', function(req, res, next) {
   var d = new Date().toISOString().substring(0, 16);
@@ -17,3 +33,8 @@ router.get('/getInquiricoesList', function(req, res, next) {
 });
 
 module.exports = router;
+
+router.get('/login', function(req, res){
+  var data = new Date().toISOString().substring(0,16) 
+  res.render('login', {d: data})
+})
